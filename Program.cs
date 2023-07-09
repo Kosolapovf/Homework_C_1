37f46dd99eadbe8d48da5ef60f723d7821d50fe1
Egor <EgorrKosolapoff@ya.ru>
2023-07-09 15:47:08 +0700

    Решение 1 задачи в ветке hw1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.Clear();
+Console.Write("Веедите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Веедите число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.Write(a);
+else
+    Console.Write(b);
