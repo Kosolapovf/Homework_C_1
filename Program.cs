5d401446ae2c2fd6bab6280793904eeb280a5826
Egor <EgorrKosolapoff@ya.ru>
2023-07-09 16:13:43 +0700

    Решение 3 задачи в ветке hw3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = number % 2;
+if (i == 0)
+    Console.Write($"{number} - четное");
+else
+    Console.Write($"{number} - нечетное");    
+
